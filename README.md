c34b55eabf2021963895127a02a1f556e162ef59
prewk <oskar.thornblad@gmail.com>
2014-04-27 17:18:12 +0200

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
 xml-faker
 =========
 
-Create fake XML with fzaninotto/Faker
+Create fake XML with the help of [fzaninotto/Faker](https://github.com/fzaninotto/Faker)
+
+Why?
+----
+
+Useful for testing stuff.
+
+Usage
+-----
+
+Supply the constructor with a valid `SimpleXMLElement` and use the methods to generate XML in the sizes that you wish.
+
+Your supplied XML will be used as a blueprint for creating the random XML. If an attribute as an integer, random integers will be created for those attributes. If your node text consists of a text with linebreaks, a random text with the same amount of linebreaks will be created for those nodes.
+
+Only the first node encountered will be used like this, the rest of your supplied XML will be ignored.
+
+Installation
+------------
+
+composer.json:
+
+    "require": {
+        "prewk/xml-faker": "*"
+    }
+
+Example
+-------
+
+Create an example.xml:
+
+    <root-node>
+      <node>
+        <child-a>OneWord</child-a>
+        <child-b>123</child-b>
+        <child-c an-integer-attribute="123">
+          <grandchild an-float-attribute="456">
+            This is some
+            text on some
+            lines
+          </grandchild>
+        </child-c>
+      </node>
+    </root-node>
+    
+Load it:
+
+    $myXmlBlueprint = simplexml_load_file("example.xml");
+    $xmlFaker = new Prewk\XmlFaker($myXmlBlueprint);
+
+Create an XML string with 100 nodes:
+
+    echo $xmlFaker->asString(Prewk\XmlFaker::NODE_COUNT_RESTRICTION_MODE, 100);
+
+Create an XML string of maximum 1 MB (1024 * 1024 chars):
+
+    echo $xmlFaker->asString(Prewk\XmlFaker::BYTE_COUNT_RESTRICTION_MODE, 1024 * 1024);
+    
+More useful, stream an XML file of 100 MB to disk (will take a while):
+
+    $xmlFaker->asFile("my-new-large-xml-file.xml", Prewk\XmlFaker::BYTE_COUNT_RESTRICTION_MODE, 100 * 1024 * 1024);
---
+++ src/XmlFaker.php
@@ -0,0 +1,135 @@
+<?php namespace Prewk;
+
+class XmlFaker
+{
+    const BYTE_COUNT_RESTRICTION_MODE = 0;
+    const NODE_COUNT_RESTRICTION_MODE = 1;
+
+    private $blueprint;
+    private $faker;
+
+    private $rootNode = "root-node";
+
+    public function __construct(\SimpleXMLElement $blueprint) {
+        $this->blueprint = $blueprint;
+        $this->faker = \Faker\Factory::create();
+    }
+
+    private function escape($in)
+    {
+        return str_replace(array("&", "<", ">", "\""), array("&amp;", "&lt;", "&gt;", "&quot;"), $in);
+    }
+
+    private function getFakeValue($original) {
+        if (is_numeric($original)) {
+            if (strpos($original, ".") !== false) {
+                return $this->faker->randomFloat();
+            } else {
+                return $this->faker->randomNumber(strlen($original));
+            }
+        } else {
+            $wordCount = str_word_count($original);
+            $newLineCount = substr_count($original, "\n");
+
+            if ($newLineCount > 0 && $wordCount > 5) {
+                return implode("\n", $this->faker->paragraphs($newLineCount));
+            } else {
+                return implode(" ", $this->faker->words($wordCount));
+            }
+        }
+    }
+
+    private function traverse($node)
+    {
+        $output = "";
+
+        foreach ($node as $child => $childNode) {
+            $attributes = $childNode->attributes();
+            $children = $childNode->children();
+
+            $fakeAttributes = array();
+
+            foreach ($attributes as $key => $value) {
+                $fakeAttributes[] = "$key=\"" . $this->escape($this->getFakeValue((string)$value)) . "\"";
+            }
+
+            $output .= count($fakeAttributes) == 0 ? "<$child>" : "<$child " . implode(" ", $fakeAttributes) . ">";
+            if (count($children) > 0) {
+                $output .= $this->traverse($childNode);
+            } else {
+                $output .= $this->escape($this->getFakeValue((string)$childNode));
+            }
+            $output .= "</$child>";
+        }
+
+        return $output;
+    }
+
+    private function extractFirstChild() {
+        foreach ($this->blueprint as $nodeName => $node) {
+            return array($node, $nodeName);
+        }
+    }
+
+    private function writer($mode, $restriction, $callback, $params)
+    {
+        list($bluePrintNode, $bluePrintNodeName) = $this->extractFirstChild();
+
+        call_user_func_array($callback, array_merge(array("<{$this->rootNode}>"), $params));
+
+        if ($mode == self::NODE_COUNT_RESTRICTION_MODE) {
+            for ($i = 0; $i < $restriction; $i++) {
+                $nextNode = "<$bluePrintNodeName>" . $this->traverse($bluePrintNode) . "</$bluePrintNodeName>";
+                call_user_func_array($callback, array_merge(array($nextNode), $params));
+            }
+        } else if ($mode == self::BYTE_COUNT_RESTRICTION_MODE) {
+            $byteCounter = (strlen($this->rootNode) * 2) + 5;
+
+            while (true) {
+                $nextNode = "<$bluePrintNodeName>" . $this->traverse($bluePrintNode) . "</$bluePrintNodeName>";
+                $upComingByteCount = strlen($nextNode);
+
+                if ($byteCounter + $upComingByteCount > $restriction) {
+                    break;
+                }
+
+                call_user_func_array($callback, array_merge(array($nextNode), $params));
+
+                $byteCounter += $upComingByteCount;
+            }
+        }
+
+        call_user_func_array($callback, array_merge(array("</{$this->rootNode}>"), $params));
+    }
+
+    public function asString($mode, $restriction)
+    {   
+        list($bluePrintNode, $bluePrintNodeName) = $this->extractFirstChild();
+
+        $outputStr = "";
+
+        $this->writer($mode, $restriction, function($xmlData, &$outputStr) {
+            $outputStr .= $xmlData;
+        }, array(&$outputStr));
+
+        return $outputStr;
+    }
+
+    public function asFile($filename, $mode, $restriction)
+    {
+        list($bluePrintNode, $bluePrintNodeName) = $this->extractFirstChild();
+
+        $handle = fopen($filename, "w");
+
+        if ($handle === false) {
+            throw new \Exception("Couldn't open file handle");
+        }
+
+        $this->writer($mode, $restriction, function($xmlData, $handle) {
+            fwrite($handle, $xmlData);
+        }, array($handle));
+
+        fclose($handle);
+    }
+}
+
